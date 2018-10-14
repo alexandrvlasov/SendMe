a295030064a3f3ed1f2d5ffa2173a7c6e6dba65b
Alexandr Korsia <alexandrkorsia@gmail.com>
2018-10-15 00:03:34 +0300

    Upgrade files readdir

--- routes/auth.js
+++ routes/auth.js
@@ -61,12 +61,10 @@ router.post('/login', (req, res) => {
             res.status(500).json({ message: 'Auth failed' }) 
         } else {
             bcrypt.compare(req.body.password, user[0].password, (error, result) => {
-                if (error) { 
-                    return res.status(401).json({ message: 'Password incorectly' })
-                }
+                if (error) return res.status(401).json({ message: 'Password incorectly' })
         
                 if (result) {
-                  const token = jwt.sign({ userId: user[0]._id, email: user[0].email }, config.secret, // ??
+                  const token = jwt.sign({ userId: user[0]._id }, config.secret, // ??
                   { expiresIn: "1h"})
         
                   return res.status(200).json({
--- routes/file.js
+++ routes/file.js
@@ -5,21 +5,13 @@ const fs = require('fs')
 const config = require('../config/app-config')
 
 router.get('/:userId', (req, res) => {
-    const userId = req.params.userId // req.body.userid
-    var files = ['fiele','user']
-    var count = 0
+    const userId = req.params.userId
 
     fs.readdir(config.dirName + '/' + userId, (err, files) => {
-        files.forEach(file => {
-            //file.
-            console.log(file)
-            files.push(file)
-            // files[count] = file
-            // count++
-        })
-    })
+        if (err) return res.status(500).json({ message: 'Problem with fs files' })
 
-    res.status(200).json({ files: files })
+        res.status(200).json({ files: files })
+    })    
 })
 
 router.post('/add', (req, res) => {
